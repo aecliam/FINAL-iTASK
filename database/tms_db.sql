384defa118601267356d58a913ee636bb5f49ec5
xHazzie <96367941+xHazzie@users.noreply.github.com>
2023-11-21 12:48:18 +0800

    Add files via upload

--- database/tms_db.sql
+++ database/tms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2023 at 10:13 PM
+-- Generation Time: Nov 21, 2023 at 05:47 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,51 @@ SET time_zone = "+00:00";
 --
 -- Database: `tms_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calendar_event_master`
+--
+
+CREATE TABLE `calendar_event_master` (
+  `event_id` int(11) NOT NULL,
+  `event_name` varchar(255) DEFAULT NULL,
+  `event_start_date` date DEFAULT NULL,
+  `event_end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `calendar_event_master`
+--
+
+INSERT INTO `calendar_event_master` (`event_id`, `event_name`, `event_start_date`, `event_end_date`) VALUES
+(1, 'test', '2023-11-02', '2023-11-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_archive`
+--
+
+CREATE TABLE `project_archive` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(2) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `manager_id` int(30) NOT NULL,
+  `user_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project_archive`
+--
+
+INSERT INTO `project_archive` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
+(6, 'test', 'test', 0, '2023-11-23', '2023-11-30', 6, '4,5,3', '2023-11-21 11:25:22');
 
 -- --------------------------------------------------------
 
@@ -44,8 +89,8 @@ CREATE TABLE `project_list` (
 --
 
 INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
-(1, 'Sample Project', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;							', 0, '2020-11-03', '2021-01-20', 2, '3,4,5', '2020-12-03 09:56:56'),
-(2, 'Sample Project 102', 'Sample Only', 0, '2020-12-02', '2020-12-31', 2, '3', '2020-12-03 13:51:54');
+(1, 'Sample Project', '																				&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;																	', 3, '2023-11-12', '2023-11-19', 0, '4,5,3', '2020-12-03 09:56:56'),
+(4, 'test', '																		test															', 0, '2023-11-21', '2023-11-30', 6, '4,3', '2023-11-19 14:23:20');
 
 -- --------------------------------------------------------
 
@@ -77,14 +122,6 @@ CREATE TABLE `scholars_clg` (
   `payslip` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `scholars_clg`
---
-
-INSERT INTO `scholars_clg` (`id`, `scholar_id`, `lastname`, `firstname`, `middlename`, `suffix`, `bdate`, `gender`, `contact`, `address`, `email`, `school`, `gradelvl`, `pgname`, `pgcontact`, `avatar`, `registration`, `report_card`, `valid_id`, `bgy_cert`, `payslip`) VALUES
-(1, 'YNW629413708', 'Lopez', 'Lorenz', 'Dumayaw', 'Jr.', '2010-07-11', 'Male', '09923262065', 'Blk. 3 Lot 12 Tiangge Subdivision, Brgy Tinaytay, Taytay, Rizal,1852', 'lopez@gmail.com', 'Knights & Archers Montesorri School', 'Grade 8', 'Florita Lopez', '+639285192849', '334057253_149943927928435_4474569998683793041_n.jpg', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png'),
-(9, 'OYT489073265', 'Dela Cruz', 'Mario', 'Sanchez', 'Jr.', '2012-11-27', 'Male', '+6309552950154', 'Blk4 Lot18 Sinampalukan St., Tinaligay Ave., Taytay, Rizal', 'marimar@gmail.com', 'Siena College of Taytay', 'Grade 10', 'Tasio Dela Cruz', '+63882759301', '334057253_149943927928435_4474569998683793041_n.jpg', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png');
-
 -- --------------------------------------------------------
 
 --
@@ -114,14 +151,6 @@ CREATE TABLE `scholars_gs` (
   `bgy_cert` varchar(200) NOT NULL,
   `payslip` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `scholars_gs`
---
-
-INSERT INTO `scholars_gs` (`id`, `scholar_id`, `lastname`, `firstname`, `middlename`, `suffix`, `bdate`, `gender`, `contact`, `address`, `email`, `school`, `gradelvl`, `pgname`, `pgcontact`, `avatar`, `registration`, `report_card`, `valid_id`, `bgy_cert`, `payslip`) VALUES
-(1, 'YNW629413708', 'Lopez', 'Lorenz', 'Dumayaw', 'Jr.', '2010-07-11', 'Male', '09923262065', 'Blk. 3 Lot 12 Tiangge Subdivision, Brgy Tinaytay, Taytay, Rizal,1852', 'lopez@gmail.com', 'Knights & Archers Montesorri School', 'Grade 8', 'Florita Lopez', '+639285192849', '334057253_149943927928435_4474569998683793041_n.jpg', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png'),
-(9, 'OYT489073265', 'Dela Cruz', 'Mario', 'Sanchez', 'Jr.', '2012-11-27', 'Male', '+6309552950154', 'Blk4 Lot18 Sinampalukan St., Tinaligay Ave., Taytay, Rizal', 'marimar@gmail.com', 'Siena College of Taytay', 'Grade 10', 'Tasio Dela Cruz', '+63882759301', '334057253_149943927928435_4474569998683793041_n.jpg', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png');
 
 -- --------------------------------------------------------
 
@@ -158,7 +187,6 @@ CREATE TABLE `scholars_jhs` (
 --
 
 INSERT INTO `scholars_jhs` (`id`, `scholar_id`, `lastname`, `firstname`, `middlename`, `suffix`, `bdate`, `gender`, `contact`, `address`, `email`, `school`, `gradelvl`, `pgname`, `pgcontact`, `avatar`, `registration`, `report_card`, `valid_id`, `bgy_cert`, `payslip`) VALUES
-(1, 'YNW629413708', 'Lopez', 'Lorenz', 'Dumayaw', 'Jr.', '2010-07-11', 'Male', '09923262065', 'Blk. 3 Lot 12 Tiangge Subdivision, Brgy Tinaytay, Taytay, Rizal,1852', 'lopez@gmail.com', 'Knights & Archers Montesorri School', 'Grade 8', 'Florita Lopez', '+639285192849', '334057253_149943927928435_4474569998683793041_n.jpg', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png'),
 (9, 'OYT489073265', 'Dela Cruz', 'Mario', 'Sanchez', 'Jr.', '2012-11-27', 'Male', '+6309552950154', 'Blk4 Lot18 Sinampalukan St., Tinaligay Ave., Taytay, Rizal', 'marimar@gmail.com', 'Siena College of Taytay', 'Grade 10', 'Tasio Dela Cruz', '+63882759301', '334057253_149943927928435_4474569998683793041_n.jpg', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png');
 
 -- --------------------------------------------------------
@@ -190,14 +218,6 @@ CREATE TABLE `scholars_shs` (
   `bgy_cert` varchar(200) NOT NULL,
   `payslip` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `scholars_shs`
---
-
-INSERT INTO `scholars_shs` (`id`, `scholar_id`, `lastname`, `firstname`, `middlename`, `suffix`, `bdate`, `gender`, `contact`, `address`, `email`, `school`, `gradelvl`, `pgname`, `pgcontact`, `avatar`, `registration`, `report_card`, `valid_id`, `bgy_cert`, `payslip`) VALUES
-(1, 'YNW629413708', 'Lopez', 'Lorenz', 'Dumayaw', 'Jr.', '2010-07-11', 'Male', '09923262065', 'Blk. 3 Lot 12 Tiangge Subdivision, Brgy Tinaytay, Taytay, Rizal,1852', 'lopez@gmail.com', 'Knights & Archers Montesorri School', 'Grade 8', 'Florita Lopez', '+639285192849', '334057253_149943927928435_4474569998683793041_n.jpg', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png', 'no-image-available.png'),
-(9, 'OYT489073265', 'Dela Cruz', 'Mario', 'Sanchez', 'Jr.', '2012-11-27', 'Male', '+6309552950154', 'Blk4 Lot18 Sinampalukan St., Tinaligay Ave., Taytay, Rizal', 'marimar@gmail.com', 'Siena College of Taytay', 'Grade 10', 'Tasio Dela Cruz', '+63882759301', '334057253_149943927928435_4474569998683793041_n.jpg', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png', 'Signature.png');
 
 -- --------------------------------------------------------
 
@@ -244,7 +264,9 @@ INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `d
 (1, 1, 'Sample Task 1', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Fusce ullamcorper mattis semper. Nunc vel risus ipsum. Sed maximus dapibus nisl non laoreet. Pellentesque quis mauris odio. Donec fermentum facilisis odio, sit amet aliquet purus scelerisque eget.&amp;nbsp;&lt;/span&gt;													', 3, '2020-12-03 11:08:58'),
 (2, 1, 'Sample Task 2', 'Sample Task 2							', 1, '2020-12-03 13:50:15'),
 (3, 2, 'Task Test', 'Sample', 1, '2020-12-03 13:52:25'),
-(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40');
+(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40'),
+(5, 4, 'test', '				test			', 2, '2023-11-19 14:24:21'),
+(6, 4, 'test12', 'test12', 3, '2023-11-19 14:28:23');
 
 -- --------------------------------------------------------
 
@@ -271,7 +293,8 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`,
 (1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
 (3, 'Marijo', 'Nieva', 'marijo@sample.com', '85ad547ba877151ac1d3d0663b6f2899', 3, '1606958760_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 09:26:42'),
 (4, 'Aedia Clint', 'Mirandilla', 'acm@sample.com', '036914837fb362f4070592a2b3a3b70d', 3, '1606963560_avatar.jpg', '2020-12-03 10:46:41'),
-(5, 'Cathlyn Joy', 'De Leon', 'cath@sample.com', 'be5b4940eee2dc7cee2619dd66d8e73a', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06');
+(5, 'Cathlyn Joy', 'De Leon', 'cath@sample.com', 'be5b4940eee2dc7cee2619dd66d8e73a', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06'),
+(6, 'Jose', 'Manalo', 'jose@acct.com', '90e528618534d005b1a7e7b7a367813f', 2, 'no-image-available.png', '2023-11-07 23:12:07');
 
 -- --------------------------------------------------------
 
@@ -304,6 +327,18 @@ INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subj
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `calendar_event_master`
+--
+ALTER TABLE `calendar_event_master`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `project_archive`
+--
+ALTER TABLE `project_archive`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `project_list`
@@ -364,10 +399,22 @@ ALTER TABLE `user_productivity`
 --
 
 --
+-- AUTO_INCREMENT for table `calendar_event_master`
+--
+ALTER TABLE `calendar_event_master`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `project_archive`
+--
+ALTER TABLE `project_archive`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `scholars_clg`
@@ -385,7 +432,7 @@ ALTER TABLE `scholars_gs`
 -- AUTO_INCREMENT for table `scholars_jhs`
 --
 ALTER TABLE `scholars_jhs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `scholars_shs`
@@ -403,13 +450,13 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_productivity`
